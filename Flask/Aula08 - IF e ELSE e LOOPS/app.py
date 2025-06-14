44a23b60472f1a952c6aa7c77e295672fb6336d6
dionisio300 <dionisioassis80@gmail.com>
2025-06-14 18:38:54 -0300

    Aula 8

--- Flask/Aula08 - IF e ELSE e LOOPS/app.py
+++ Flask/Aula08 - IF e ELSE e LOOPS/app.py
@@ -2,20 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def paginaInicial():
-    return render_template('index.html')
-
-@app.route('/listarEventos')
-def paginaListarEventos():
-    return render_template('listarEventos.html')
-
-@app.route('/paginaConsultar', methods = ['get','post'])
-def paginaConsultar():
-
-    mostrarResultado = False
-
-    dadosEvento = [{
+dadosEvento = [{
         "nome":'Show de Samba',
         "data":'02/05/2026',
         "local":"Praia",
@@ -40,6 +27,23 @@ def paginaConsultar():
         "ingressosVendidos":2635,
         "ingressosDisponiveis":365
     }]
+
+@app.route('/')
+def paginaInicial():
+    return render_template('index.html')
+
+@app.route('/listarEventos')
+def paginaListarEventos():
+
+    return render_template('listarEventos.html',eventos=dadosEvento)
+
+@app.route('/paginaDeletar', methods = ['get','delete'])
+def paginaDeletar():
+    return ('Deletar')
+
+@app.route('/paginaConsultar', methods = ['get','post'])
+def paginaConsultar():
+    mostrarResultado = False
     resultadoEvento = {}
     if request.method == 'POST':
         id = int(request.form.get('id_busca'))
@@ -62,8 +66,32 @@ def paginaConsultar():
     # Youth Alunos
     # senha: Youthalunos*.
 
-@app.route('/paginaCadastrar')
+@app.route('/paginaCadastrar', methods = ['get','post'])
 def paginaCadastrar():
+    if request.method == 'POST':
+       nome = request.form.get('nome')
+       data = request.form.get('data')
+       local = request.form.get('local')
+       hora = request.form.get('hora')
+       lotacao = int(request.form.get('lotacao'))
+       ingressosVendidos = 0
+       ingressosDisponiveis = lotacao - ingressosVendidos
+       
+       novoEvento = {
+           "nome":nome,
+           "data":data,
+           "local":local,
+           "hora":hora,
+           "lotacao":lotacao,
+           "ingressosVendidos":ingressosVendidos,
+           "ingressosDisponiveis":ingressosDisponiveis
+       }
+
+       dadosEvento.append(novoEvento)
+       print(dadosEvento)
+
+       print(nome,data,local,hora,lotacao)
+
     return render_template('paginaCadastrar.html')
 
 @app.route('/paginaAtualizar')
