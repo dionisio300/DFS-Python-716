32ceb572f6d718eceddc52b39ef6b4036fd36646
dionisio300 <dionisioassis80@gmail.com>
2025-08-02 19:56:57 -0300

    aula13

--- Flask/Aula13/Aula/app.py
+++ Flask/Aula13/Aula/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import mysql.connector as my
+
 app = Flask(__name__)
+app.secret_key = '12345'
+
 
 # terminal: pip install mysql-connector-python
 
@@ -38,14 +41,101 @@ def login():
         sql = "select * from usuarios where email = %s"
         cursor.execute(sql,(email,))
         usuario_buscado = cursor.fetchone()
+        try:
+            if senha == usuario_buscado["senha"]:
+                print('Usuário pode entrar')
 
+                session["nome"] = usuario_buscado["nome"]
+                session["tipo"] = usuario_buscado["tipo"]
+                session["logado"] = True
+
+
+                if usuario_buscado["tipo"] == 'administrador':
+                    return redirect('paginaAdm')
+                elif usuario_buscado["tipo"] == 'cliente':
+                    return redirect('paginaCliente')
+                elif usuario_buscado["tipo"] == 'usuario':
+                    return redirect('paginaUsuario')
+
+            elif senha != usuario_buscado["senha"]:
+                print('Usuário erro a senha, tente novamente!')
+        except:
+            print('erro ao trazer do banco de dados')
         
 
         print(usuario_buscado)
         return render_template('login.html')
 
 
+@app.get('/paginaAdm')
+def administrador():
+    if 'tipo' not in session:
+        return redirect('login')
+    
+    if session['tipo'] == 'administrador':
+        return render_template('administrador.html')
+    else:
+        return redirect('login')
+        
+
+@app.get('/paginaCliente')
+def cliente():
+    if 'tipo' not in session:
+        return redirect('login')
+    if session['tipo'] == 'cliente':
+        return render_template('cliente.html')
+    else:
+        return redirect('login')
+
+@app.get('/paginaUsuario')
+def usuario():
+    if 'tipo' not in session:
+        return redirect('login')
+    if session['tipo'] == 'usuario':
+        #Aqui vai o código para essa página
+
+        return render_template('usuario.html')
+    else:
+        return redirect('login')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'GET':
+        return render_template('cadastro.html') 
+    elif request.method == 'POST':
+        nome = request.form.get('name')
+        nome_usuario = request.form.get('username')
+        email = request.form.get('email')
+        tipo = request.form.get('accountType')
+        senha = request.form.get('senha')
+        print(f'Name: {nome}, Username: {nome_usuario}, AccountType: {tipo}, E-mail: {email}, Senha: {senha}')
+        conexao = conectar_banco()
+        cursor = conexao.cursor(dictionary=True)
+        sql_verificacao = "SELECT * FROM usuarios WHERE email = %s"
+        cursor.execute(sql_verificacao, (email,))
+        usuario_buscado = cursor.fetchone()
+        if usuario_buscado:
+            return "E-mail já cadastrado. Tente outro."
+        sql_insercao = """
+            INSERT INTO usuarios (nome, nome_usuario, email, tipo, senha)
+            VALUES (%s, %s, %s, %s, %s)
+        """
+        cursor.execute(sql_insercao, (nome, nome_usuario, email, tipo, senha))
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+
+        return "Usuário cadastrado com sucesso"
+
+    
+ 
+app.run(debug=True)
 
 
+# criar a rota
+# criar o template
+# pegar as informações do formulário e levar para o python
 
 app.run(debug=True)
+
+
