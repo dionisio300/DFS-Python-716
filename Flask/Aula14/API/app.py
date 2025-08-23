ffb1ad758ac39fbe801332569ea5338da816d10b
andreyqpi <andreytome2@gmail.com>
2025-08-23 18:55:17 -0300

    Aula APIs

--- Flask/Aula14/API/app.py
+++ Flask/Aula14/API/app.py
@@ -10,29 +10,25 @@ def paginaInicial():
 
 @app.route('/mostrarEndereco', methods = ['GET','POST'])
 def mostrarEndereco():
+
     if request.method == 'POST':
         nome = request.form.get('nome')
         cep = request.form.get('cep')
         # Criar a URL da API
         url = f'https://viacep.com.br/ws/{cep}/json/'
-
         # Fazer a chamada da API
         resposta = requests.get(url)
-        
         # Validação da resposta
         if resposta.status_code == 200:
-            # Trazer os dados vindos da API para a variável dados
             dados = resposta.json()
-
-            if dados['erro']:
-                print('Erro')
-                return render_template('index.html',nome=nome,cep=cep,resposta = 'erro')
+            print(dados)
+            if 'erro' in dados:
+                return render_template('paginaErro.html')
             else:
-                # Enviando os dados para a página HTML
-                return render_template('index.html',nome=nome,cep=cep,resposta = dados)
+                return render_template('index.html', dados=dados)
         else:
             erro = resposta.status_code
-            return render_template('index.html', erro=erro)
+            return render_template('paginaErro.html')
     else:
         return render_template('index.html')
 
